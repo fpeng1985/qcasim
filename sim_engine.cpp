c305b04ae785dc9a4f54f2393c504b9a33738ed8
Fei Peng <fpeng1985@gmail.com>
2016-10-14 11:17:24 +0800

    add Polarization type

--- sim_engine.cpp
+++ sim_engine.cpp
@@ -6,8 +6,29 @@
 
 namespace hfut {
 
-    void SimEngine::initialize_circuit() {
-        circuit->initialize_polarization();
+    SimEngine::TEMPERATURE = 1;
+
+    SimEngine::SimEngine(QCACircuit *c, const std::vector<Polarization> &in_p) :circuit(c) {
+        input_p.assign(in_p.begin(), in_p.end());
+
+    }
+
+    void SimEngine::initialize_output_polarization() {
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_real_distribution<> dis(-1, 1);
+
+        for (auto &line : circuit->cells) {
+            for (auto &cell : line) {
+                if (cell != nullptr && cell->cell_type != CellType::Input) {
+                    cell->polarization = dis(gen);
+                }
+            }
+        }
+    }
+
+    void SimEngine::generate_neighbour() {
+
     }
 
 }
--- sim_engine.h
+++ sim_engine.h
@@ -7,17 +7,29 @@
 
 #include "qca_circuit.h"
 
+#include <vector>
+#include <tuple>
 
 namespace hfut {
 
+    typedef std::tuple<size_t, size_t, double> Polarization;
+
     class SimEngine {
+
     public:
-        void initialize_circuit();
-        void create_neighbour();
+        SimEngine(QCACircuit *circuit, const std::vector<Polarization> &input_p);
+
+        void initialize_output_polarization();
+        void generate_neighbour_polarization();
 
     private:
-        QCACircuit *circuit;
+        static const double TEMPERATURE;
 
+        QCACircuit *circuit;
+        std::vector<Polarization> input_p;
+        std::vector<Polarization> output_p;
+        std::vector<Polarization> neighbour_p;
+        std::vector<Polarization> best_p;
     };
 
 }
